50284d8530fe4624e79934c5398e2e23c759aeaa
Artur Klimov <imbalance1997@gmail.com>
2020-05-04 17:07:13 +0300

    Setup of EF and Identity

---
+++ Contest.DA/ContestContext.cs
@@ -0,0 +1,14 @@
+﻿using Contest.DA.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contest.DA
+{
+    public class ContestContext : IdentityDbContext<User>
+    {
+        public ContestContext(DbContextOptions<ContestContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- Contest.Web/Startup.cs
+++ Contest.Web/Startup.cs
@@ -1,10 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Contest.DA;
+using Contest.DA.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,9 +12,25 @@ namespace Contest.Web
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
+        private IConfiguration _configuration;
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
             services.AddMvc();
+
+            services.AddDbContext<ContestContext>(options => options.UseSqlServer(configuration.GetConnectionString("ContestContext")));
+
+            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ContestContext>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -24,7 +40,11 @@ namespace Contest.Web
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
